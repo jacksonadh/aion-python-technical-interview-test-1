e833a8395c033b06aa8e64d90dd53760dbf90dd9
LawsonDaiki <lawsondaiki@gmail.com>
2022-04-19 08:18:17 -0300

    created problem statement

--- test.py
+++ test.py
@@ -2,4 +2,20 @@ from product_db import product_db
 from sale_order_db import sale_order_db
 
 def get_kpis(selected_date_from, selected_date_to):
-	print(sale_order_db[0])
+	# Your goal is to develop a function that returns the following kpis between two given dates as inputs:
+	# - total revenue
+	# - average ticket (average value of a sale order)
+	# - margin (total gross cost / total revenue)
+	
+	kpis = {
+		'total_revenue': None,
+		'average_ticket': None,
+		'margin': None
+	}
+
+	# write your solution here
+
+	return kpis
+
+kpis = get_kpis('2021-07-1', '2021-07-8')
+print(kpis)
