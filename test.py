38f6d97a893c04e0a695d6401120feb9d89594b3
LawsonDaiki <lawsondaiki@gmail.com>
2022-04-19 15:02:31 -0300

    now only two kpis (total revenue and cancellation); formatted db

--- test.py
+++ test.py
@@ -1,26 +1,30 @@
+from datetime import datetime
+
 from product_db import product_db
 from sale_order_db import sale_order_db
 
 def get_kpis(selected_date_from, selected_date_to):
 	# Your goal is to develop a function that returns the following kpis between two given dates as inputs:
 	# - total revenue
-	# - average ticket (average amount of revenue per sale)
-	# - margin (total gross cost / total revenue)
+	# - cancellation (canceled sale orders / total amount of sale orders)
 	# The variable product_db contains a list of all products, where each item is a dictionary with the product's id, full price and gross cost.
 	# And sale_order_db contains a list of sale orders, where each item is a dictionary with the sale's id, customer's id, a list of purchased products, sale's date and cancelation's date (if there was any).
+
+	# A little help:
+	selected_date_from = datetime.strptime(selected_date_from, '%Y-%m-%d')
+	selected_date_to = datetime.strptime(selected_date_to, '%Y-%m-%d')
+
+	# Write your solution here
 
 	# Return the kpi values following the variable format below:
 	kpis = {
 		'total_revenue': None,
-		'average_ticket': None,
-		'margin': None
+		'cancellation': None,
 	}
-
-	# Write your solution here
 
 	return kpis
 
-kpis = get_kpis('2021-07-1', '2021-07-8')
+kpis = get_kpis('2021-07-01', '2021-07-08')
 print(kpis)
 # To run this file just type the command below in the console:
 # python test.py
